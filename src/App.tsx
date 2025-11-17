1b86e507e7c68ad346615a50b302413df8c19807
fbadea <fbadea@sorint.com>
2025-11-17 10:22:45 +0200

    Refactor App component structure and update TypeScript configuration for deprecation handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useLocation } from "react-router-dom";
 import AppRoutes from "@/routes/AppRoutes";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const location = useLocation();
@@ -19,13 +18,4 @@ function App() {
     </div>
   );
 }
-
-function AppWrapper() {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
-
-export default AppWrapper;
+export default App;
